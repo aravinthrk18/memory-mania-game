ed071610c721e188b1425d7aee1b6f326dd9191c
aravinthrk18 <aravinthkannan6669@gmail.com>
2026-01-26 23:33:15 +0530

    AUTHOR NAMR

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,35 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+
+        {/* Author credit */}
+        <footer className="author">
+          <a
+            href="https://www.linkedin.com/in/aravinth-rk-88b7a7291/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           With ❤️  Aravinth Kannan
+          </a>
+        </footer>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
+
